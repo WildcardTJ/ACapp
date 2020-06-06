1c82c5f61a0737db36ba1d99d48ca3e8f8ec23b2
unknown <tomjurewicz@hotmail.co.uk>
2020-06-06 14:46:23 +0100

    added language enum to accountmodel

--- app/src/main/java/com/example/animalcrossing/ui/insect/model/AccountModel.kt
+++ app/src/main/java/com/example/animalcrossing/ui/insect/model/AccountModel.kt
@@ -7,6 +7,25 @@ enum class Hemisphere {
     SOUTHERN
 }
 
+enum class Language {
+    EU_EN,
+    EU_DE,
+    EU_ES,
+    EU_FR,
+    EU_IT,
+    EU_NL,
+    EU_RU,
+    US_EN,
+    US_ES,
+    US_FR,
+    CN_ZH,
+    TW_ZH,
+    JP_JA,
+    KR_KO
+}
+
+
 data class AccountModel(
-    var hemisphere: Hemisphere = Hemisphere.NORTHERN
+    var hemisphere: Hemisphere = Hemisphere.NORTHERN,
+    var language: Language = Language.EU_EN
 ): Serializable
